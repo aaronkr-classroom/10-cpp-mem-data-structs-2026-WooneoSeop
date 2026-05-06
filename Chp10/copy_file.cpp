3c65342e72d937a20cc5be6a362cd9be25853fe9
ththfql <145094239+ththfql@users.noreply.github.com>
2026-05-06 17:07:39 +0900

    .

---
+++ Chp10/copy_file.cpp
@@ -0,0 +1,18 @@
+﻿// copy_file.cpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ifstream infile("in.txt");
+	ofstream outfile("out-again.txt");
+
+	string s;
+
+	while (getline(infile, s))
+		outfile << s << endl;
+	return 0;
+}
